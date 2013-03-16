5c75f9f14bf44d4bf3926316eeea1cfe3cb8f1fa
Glenn Nagel <glenn@beenverified.com>
2013-03-16 14:42:01 -0400

    Initial commit of Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoksboxForRails::Application.config.secret_token = '1d3cb453c167ca2290895b17d0ebf046328fd27ba82f944ac90a3beab0bdfc96abfbf4d8ba26af616113ddff7d3cc6379e27bb93de99faae06597521bd2488ec'
